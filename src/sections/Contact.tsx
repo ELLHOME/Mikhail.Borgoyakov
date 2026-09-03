bbfdf8b8dacf59096b10e4c6d8cb389f6b4c36d1
Mikhail Borgoyakov <mihalkhakas@gmail.com>
2026-09-03 14:27:58 +0300

    Add Telegram and GitHub contact links

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -9,8 +9,8 @@ export default function Contact() {
       <h2 className="sec-title reveal" style={{ fontWeight: 200 }}>{t.title}</h2>
       <a className="c-mail" href="mailto:ellhome@yandex.ru">ellhome@yandex.ru</a>
       <div className="c-links reveal">
-        <a href="#">TELEGRAM</a>
-        <a href="#">GITHUB</a>
+        <a href="https://t.me/M_B_lab" target="_blank" rel="noopener noreferrer">TELEGRAM</a>
+        <a href="https://github.com/ellhome" target="_blank" rel="noopener noreferrer">GITHUB</a>
       </div>
     </section>
   );
